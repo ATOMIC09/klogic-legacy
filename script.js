2d4e5226c07ba6e6b93a7d8068e0ede509391ddf
Phutawan Chanrueng <66838025+ATOMIC09@users.noreply.github.com>
2023-08-21 10:21:23 +0700

    V1.1
    Edit Disclaimer

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const disclaimerOverlay = document.getElementById('disclaimer-overlay');
     const acceptButton = document.getElementById('accept-button');
+    const declineButton = document.getElementById('decline-button');
     const originalPageContent = document.getElementById('original-page-content');
   
     // Show the disclaimer overlay
@@ -11,5 +12,9 @@ document.addEventListener('DOMContentLoaded', function () {
         // Hide the disclaimer overlay and show the original content
         disclaimerOverlay.style.display = 'none';
         originalPageContent.style.display = 'block';
+    });
+    declineButton.addEventListener('click', function () {
+        // Redirect to reg.kmutnb.ac./th
+        window.location.href = 'https://reg.kmutnb.ac.th';
     });
 });
